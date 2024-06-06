5413dfafc22685b36fe59e1c3b5643226d129058
Rafael Amaral de Oliveira <rafacienciacomputacao@gmail.com>
2024-06-06 10:01:20 -0300

    Search: using django.db.models.Q for queries with OU (OR)

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # TODO docstring
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 from blog.models import Post
 
 PER_PAGE = 9
@@ -70,6 +71,25 @@ def tag(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+
+def search(request):
+    # TODO docstring
+    search_value = request.GET.get('search', '').strip()
+    posts = Post.objects.get_published().filter(
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+    )[0:PER_PAGE]
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
 
